19c2f0b7ce50dbf6256ab1b03d06ecee643f2a6a
walkerj <wpjezek@gmail.com>
2022-04-18 17:27:12 -0600

    Adding the prompts

--- index.js
+++ index.js
@@ -3,7 +3,62 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a description for your project",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "List the necessary steps to install your project",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How should this project be used?",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "List the contribution guidelines",
+    },
+    {
+        type: "input",
+        name: "testInstruction",
+        message: "List the test intructions for your project",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select the appropriate license for your project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "GitHub",
+        message: "Add your GitHub username",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Add your email address",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,38 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - Installation
+  - Usage
+  - License
+  - Contributing
+  - Tests
+  - Questions
+
+  ## Installation
+  ${data.intallation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+  // license link
+  // license badge
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests
+  ${data.testInstructions}
+
+  ## Questions
+  ${data.GitHub}
+  ${data.email}
+
 `;
 }
 
